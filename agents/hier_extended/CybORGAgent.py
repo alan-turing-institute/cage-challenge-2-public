e3588a0efe20fd3d836e2d4d502be354958c0426
mlf20 <m.foley20@imperial.ac.uk>
2022-06-23 14:28:41 +0100

    sota bline

--- agents/hier_extended/CybORGAgent.py
+++ agents/hier_extended/CybORGAgent.py
@@ -6,7 +6,7 @@ from CybORG.Agents import B_lineAgent, GreenAgent, BaseAgent, RedMeanderAgent, B
 from CybORG.Agents.Wrappers import ChallengeWrapper
 
 
-class CybORGAgent(gym.Env):
+class CybORGCompAgent(gym.Env):
     max_steps = 100
     path = str(inspect.getfile(CybORG))
     path = path[:-10] + '/Shared/Scenarios/Scenario2.yaml'
@@ -28,10 +28,10 @@ class CybORGAgent(gym.Env):
         self.steps = 0
         self.agent_name = self.env.agent_name
         if isinstance(self.env.get_action_space(self.agent_name), list):
-            self.action_space = gym.spaces.MultiDiscrete(54)
+            self.action_space = gym.spaces.MultiDiscrete(41)
         else:
             assert isinstance(self.env.get_action_space(self.agent_name), int)
-            self.action_space = gym.spaces.Discrete(54)
+            self.action_space = gym.spaces.Discrete(41)
         self.observation_space = self.env.observation_space
         self.action = None
 
@@ -40,7 +40,7 @@ class CybORGAgent(gym.Env):
         return self.env.reset()
 
     def step(self, action=None):
-        if action > 40:
+        if action > 28:
             action += 104
         result = self.env.step(action=action)
         self.steps += 1
