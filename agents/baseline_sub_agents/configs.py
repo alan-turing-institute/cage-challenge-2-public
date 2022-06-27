b74e4f65cb00e5a9732b9ff4e4c8f0bc34e31304
mlf20 <m.foley20@imperial.ac.uk>
2022-06-27 16:39:07 +0100

    new configs

--- agents/baseline_sub_agents/configs.py
+++ agents/baseline_sub_agents/configs.py
@@ -3,10 +3,18 @@ from ray.rllib.agents.impala.impala import DEFAULT_CONFIG as IMPALA_CONFIG
 from ray.rllib.agents.a3c.a2c import A2C_DEFAULT_CONFIG
 from ray.rllib.agents.sac.sac import DEFAULT_CONFIG as SAC_CONFIG
 from ray.rllib.agents.maml.maml import DEFAULT_CONFIG as MAML_CONFIG
-
+from ray.rllib.agents.ppo.ppo import DEFAULT_CONFIG as PPO_CONFIG
+from ray.rllib.models import ModelCatalog
 from ray.rllib.agents.trainer import Trainer
 from CybORGAgent import *
+from CybORGMAML import *
 import os
+from neural_nets import *
+from ray.rllib.models.tf.attention_net import GTrXLNet
+
+ModelCatalog.register_custom_model("CybORG_Torch", TorchModel)
+ModelCatalog.register_custom_model("CybORG_TF", TFModel)
+ModelCatalog.register_custom_model("CybORG_GTrXL_Model", GTrXLNet)
 
 APEX_config = Trainer.merge_trainer_configs(
         APEX_DEFAULT_CONFIG,{
@@ -17,7 +25,7 @@ APEX_config = Trainer.merge_trainer_configs(
         # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "model": {
-            "custom_model": "CybORG_APEX_Model",
+            "custom_model": "CybORG_Torch",
             # "vf_share_layers": False,
         },
         "lr": 0.0005,
@@ -38,7 +46,7 @@ APEX_RAINBOW_config = Trainer.merge_trainer_configs(
 
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),  # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
         "model": {
-            "custom_model": "CybORG_APEX_Model",
+            "custom_model": "CybORG_Torch",
             "vf_share_layers": True,
         },
 
@@ -86,7 +94,7 @@ IMPALA_config = Trainer.merge_trainer_configs(
         # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "model": {
-            "custom_model": "CybORG_IMPALA_Model",
+            "custom_model": "CybORG_Torch",
             "vf_share_layers": False,
         },
         "lr": 0.0005,
@@ -107,7 +115,7 @@ A2C_config = Trainer.merge_trainer_configs(
         # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "model": {
-            "custom_model": "CybORG_A2C_Model",
+            "custom_model": "CybORG_Torch",
             "vf_share_layers": False,
         },
         "lr": 0.0005,
@@ -129,11 +137,11 @@ SAC_config = Trainer.merge_trainer_configs(
         # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "Q_model": {
-            "custom_model": "CybORG_SAC_Model",
+            "custom_model": "CybORG_Torch",
             # "vf_share_layers": False,
         },
         "policy_model": {
-            "custom_model": "CybORG_SAC_Model",
+            "custom_model": "CybORG_Torch",
             # "vf_share_layers": False,
         },
         "lr": 0.0005,
@@ -151,12 +159,12 @@ SAC_config = Trainer.merge_trainer_configs(
 MAML_config = Trainer.merge_trainer_configs(
     MAML_CONFIG,
     {
-        "env": CybORGAgent,
+        "env": CybORGMAMLWrapped,
 
         # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "model": {
-            "custom_model": "CybORG_MAML_Model",
+            "custom_model": "CybORG_Torch",
             # "vf_share_layers": False,
         },
         "lr": 0.0005,
@@ -169,3 +177,117 @@ MAML_config = Trainer.merge_trainer_configs(
         # "vf_clip_param": 5.0,
     }
 )
+
+GRtXL_config = Trainer.merge_trainer_configs(
+    PPO_CONFIG,
+    {
+        "env": CybORGAgent,
+        # Use GPUs iff `RLLIB_NUM_GPUS` env various set to > 0.
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "num_workers": 0,
+        "gamma": 0.99,
+        "num_envs_per_worker": 20,
+        "entropy_coeff": 0.001,
+        "vf_loss_coeff": 1e-5,
+        "num_sgd_iter": 10,
+        "model": {
+            "custom_model": "CybORG_GTrXL_Model",
+            "max_seq_len": 10,
+            "custom_model_config": {
+                "num_transformer_units": 1,
+                "attention_dim": 32,
+                "num_heads": 1,
+                "memory_inference": 5,
+                "memory_training": 5,
+                "head_dim": 32,
+                "position_wise_mlp_dim": 32,
+            },
+        },
+        # "lr": 0.0005,
+        # "momentum": tune.uniform(0, 1),
+        # "num_workers": 2,  # parallelism
+        "framework": "tf2",  # May also use "tf2", "tfe" or "torch" if supported
+        # "eager_tracing": True,  # In order to reach similar execution speed as with static-graph mode (tf default)
+        # "vf_loss_coeff": 1,  # Scales down the value function loss for better comvergence with PPO
+        # "clip_param": 0.5,
+        # "vf_clip_param": 5.0,
+    }
+)
+
+attention_config = {
+    "env": CybORGAgent,
+    "env_config": {
+        "null": 0,
+    },
+    "gamma": 0.99,
+    # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+    "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", 0)),
+    "num_envs_per_worker": 20,
+    "entropy_coeff": 0.001,
+    "num_sgd_iter": 10,
+    "vf_loss_coeff": 1e-5,
+    "model": {
+        # Attention net wrapping (for tf) can already use the native keras
+        # model versions. For torch, this will have no effect.
+        "_use_default_native_models": True,
+        "custom_model": "CybORG_Torch",
+        'fcnet_hiddens': [256, 256, 52],
+        "use_attention": not True,
+        "use_lstm":  True,
+        "max_seq_len": 10,
+        "attention_num_transformer_units": 1,
+        "attention_dim": 32,
+        "attention_memory_inference": 10,
+        "attention_memory_training": 10,
+        "attention_num_heads": 1,
+        "attention_head_dim": 32,
+        "attention_position_wise_mlp_dim": 32,
+    },
+    "framework": 'torch',
+}
+
+PPO_Curiosity_config = Trainer.merge_trainer_configs(
+    PPO_CONFIG, {
+        "env": CybORGAgent,
+        "env_config": {
+            "null": 0,
+        },
+        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "model": {
+            "custom_model": "CybORG_Torch",
+            'fcnet_hiddens': [256, 256, 256],
+            "vf_share_layers": False,
+        },
+        "lr": 0.0005,
+        # "momentum": tune.uniform(0, 1),
+        "num_workers": 0,  # parallelism
+        "framework": "torch",  # May also use "tf2", "tfe" or "torch" if supported
+        "eager_tracing": True,
+        # In order to reach similar execution speed as with static-graph mode (tf default)
+        "vf_loss_coeff": 1,  # Scales down the value function loss for better comvergence with PPO
+        "clip_param": 0.5,
+
+        "vf_clip_param": 5.0,
+        "exploration_config": {
+            "type": "Curiosity",  # <- Use the Curiosity module for exploring.
+            "eta": 1.0,  # Weight for intrinsic rewards before being added to extrinsic ones.
+            "lr": 0.001,  # Learning rate of the curiosity (ICM) module.
+            "feature_dim": 53,  # Dimensionality of the generated feature vectors.
+            # Setup of the feature net (used to encode observations into feature (latent) vectors).
+            "feature_net_config": {
+                "fcnet_hiddens": [],
+                "fcnet_activation": "relu",
+            },
+            "inverse_net_hiddens": [256],  # Hidden layers of the "inverse" model.
+            "inverse_net_activation": "relu",  # Activation of the "inverse" model.
+            "forward_net_hiddens": [256],  # Hidden layers of the "forward" model.
+            "forward_net_activation": "relu",  # Activation of the "forward" model.
+            "beta": 0.2,  # Weight for the "forward" loss (beta) over the "inverse" loss (1.0 - beta).
+            # Specify, which exploration sub-type to use (usually, the algo's "default"
+            # exploration, e.g. EpsilonGreedy for DQN, StochasticSampling for PG/SAC).
+            "sub_exploration": {
+                "type": "StochasticSampling",
+            }
+        }
+    })
