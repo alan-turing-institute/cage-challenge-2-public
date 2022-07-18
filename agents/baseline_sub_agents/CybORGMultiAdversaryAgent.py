3f337b53ab07abf526e8ac5432c6021778cf4733
mlf20 <m.foley20@imperial.ac.uk>
2022-07-18 15:50:02 +0100

    name change

--- agents/baseline_sub_agents/CybORGMultiAdversaryAgent.py
+++ agents/baseline_sub_agents/CybORGMultiAdversaryAgent.py
@@ -6,31 +6,35 @@ from CybORG.Agents import B_lineAgent, GreenAgent, BaseAgent, RedMeanderAgent, B
 from CybORG.Agents.Wrappers import ChallengeWrapper, OpenAIGymWrapper, EnumActionWrapper, FixedFlatWrapper, ReduceActionSpaceWrapper
 
 
-class CybORGAgent(gym.Env):
+class CybORGMultiAgent(gym.Env):
     max_steps = 100
     path = str(inspect.getfile(CybORG))
     path = path[:-10] + '/Shared/Scenarios/Scenario2.yaml'
 
-    agents = {
-        'Red': B_lineAgent  # , #RedMeanderAgent, 'Green': GreenAgent
-    }
 
     """The CybORGAgent env"""
 
     def __init__(self, config: EnvContext):
-        self.cyborg = CybORG(self.path, 'sim', agents=self.agents)
 
-        self.env = OpenAIGymWrapper('Blue',
-                                    EnumActionWrapper(FixedFlatWrapper(ReduceActionSpaceWrapper(self.cyborg))))
-        self.env  = ChallengeWrapper(env=self.cyborg, agent_name='Blue')
+
+        self.cyborg = CybORG(self.path, 'sim', agents={'Red':RedMeanderAgent})
+        self.RMenv  = ChallengeWrapper(env=self.cyborg, agent_name='Blue')
+        self.cyborg = CybORG(self.path, 'sim', agents={'Red':B_lineAgent})
+        self.BLenv  = ChallengeWrapper(env=self.cyborg, agent_name='Blue')
+        self.env = self.BLenv
         self.steps = 1
         self.agent_name = self.env.agent_name
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
         self.action = None
 
+
     def reset(self):
         self.steps = 1
+        if random.choice([0,1]) == 0:
+            self.env = self.BLenv
+        else:
+            self.env = self.RMenv
         return self.env.reset()
 
     def step(self, action=None):
